525f41042e5792b3a7bc6ba451e1233d49dbd572
Nicklas Thorsen <nicklas@Nicklass-MacBook-Pro.local>
2019-03-29 11:35:23 +0100

    opdateret med ejendomsprisindex

--- Dataproject.py
+++ Dataproject.py
@@ -11,13 +11,23 @@ import pydst
 Dst = pydst.Dst(lang='da')
 
 #%%
-Dst.get_subjects()
+# Fetching variables for the consumer price index in dst
+forpris_vars = Dst.get_variables(table_id='PRIS112')
+forpris_vars
 
 #%%
-boligpris_vars = Dst.get_variables(table_id='PRIS112')
-boligpris_vars
-
-#%%
+# Fetching data for the yearly average consumer price index with 2005=100
 forprisindex = Dst.get_data(table_id = 'PRIS112', variables={'HOVED':['1005'], 'tid':['*']})
 
 forprisindex.sample(10)
+#%%
+# Fetching variables for the property price index in dst
+ejpris_vars = Dst.get_variables(table_id='EJ55')
+ejpris_vars
+#%%
+ejpris_vars['values'][1][:10]
+ejpris_vars['values'][2][:10]
+ejpris_vars['values'][3][:10]
+#%%
+ejprisindex = Dst.get_data(table_id = 'EJ55', variables={'OMRÅDE':['01'], 'EJENDOMSKATE':['2103'], 'TAL':['100'], 'Tid':['*']})
+ejprisindex.sample(10)
