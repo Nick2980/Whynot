172396d80315c347a97731ce3a3d8bedb072a977
Nicklas Thorsen <nicklas@Nicklass-MacBook-Pro.local>
2019-03-29 10:43:57 +0100

    import tools and setting up API

--- Dataproject.py
+++ Dataproject.py
@@ -1,3 +1,16 @@
 #%%
-Message = 'Nu skal jeg ryge'
-print(Message)
+# Import of pandas and numpy for work and analysis on lists and arrays
+import pandas as pd
+import numpy as np
+#%%
+# import of matplot for making graphs later on in the analysis
+import matplotlib.pyplot as plt
+#%%
+# import of dst API functions. Note: requires installation of pip install git+https://github.com/elben10/pydst in terminal(mac)/cmd(windows)
+import pydst
+Dst = pydst.Dst(lang='da')
+
+#%%
+Dst.get_subjects()
+
+#%%
