d72ae8d538fc2ad022535fb730672ff839d320da
Sophus Schlosser <sophusschlosser@Sophuss-MacBook-Pro.local>
2019-04-01 12:39:23 +0200

    Update

--- present_value_function.py
+++ present_value_function.py
@@ -1,4 +1,5 @@
-def present_value(rate)
+##Present value function
+def present_value(rate):
     """ The present value of a annuity. 
     Calulates the present value of a yearly payment of 1 kr. paid in 30 years at rate x.
     Args:
@@ -9,8 +10,11 @@ def present_value(rate)
     
         y (float): present value of 1 kr. in 30 years at rate x
     """
-    
     y = (1 - (1 - rate)**-30)/r 
+
     return y
 
-    
+#%%
+print(present_value(1))
+
+
