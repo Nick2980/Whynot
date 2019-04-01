f2e57fab1593fbb19a31587837e312c3f82d4403
Sophus Schlosser <sophusschlosser@Sophuss-MacBook-Pro.local>
2019-04-01 16:00:04 +0200

    Update

--- Read morgage rates.py
+++ Read morgage rates.py
@@ -7,9 +7,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #%%
+##Present value function
+def present_value(rate):
+    """ The present value of a annuity. 
+    Calulates the present value of a yearly payment of 1 kr. paid in 30 years at rate x.
+    Args:
+        rate (float): the rate
+        
+    Returns:
+        present value of 1 kr. in 30 years at rate x
+    """
+    try:
+        return (1 - (1 + rate)**-30)/rate 
+
+    except:
+        print('Error: Check input')
+
+#%%
 #load rates from excel
 rates = pd.read_excel('Morgagebond_rates.xlsx')
 
 #%%
+#line = rates.plt.plot
+print(rates['Long_rates'])
+
+#%%
+present_value_list = present_value(rates['Long_rates'])
+
+rates.append(present_value_list)
 print(rates)
 
+#plt.scatter
+#plt.show()
